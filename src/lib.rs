080e96efb381bc1a51cf4737ee9607d92a0fe1ee
Maxym Naumchyk <max.naumch@gmail.com>
2018-12-25 23:57:27 +0200

    Reworked module

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,64 @@
+use std::sync::{
+    RwLock,
+    RwLockReadGuard,
+    RwLockWriteGuard,
+};
+
 use std::collections::{
     LinkedList,
+    BTreeMap,
 };
 
 use std::vec::Vec;
 
 use ccs::{
     ThreadKey,
+    ChannelKey,
     Network,
 };
 
+/// Information from scheduler to CPU controller of how to execute join
+/// operation.
+pub enum JoinMode {
+
+    /// New thread was assigned to core.
+    ContextChanged,
+
+    /// Thread runs for the first time.
+    NewContext,
+}
+
+/// Depending on timer type different interrupts will raise at alarm.
+pub enum TimerType {
+
+    /// Execution timeout when threads processor time elapses.
+    Execution,
+
+    /// Wait timeout when channel signal hadn't arrived before timer alarm.
+    /// Passed value indicates thread that waits.
+    ChannelWait(ThreadKey),
+}
+
 /// Thread scheduling information.
 pub struct ThreadSched {
+
+    /// Thread key to identify it in the Network.
     key: ThreadKey,
+
+    /// Additional time that was given to this thread in microseconds.
+    add_time: u32,
+}
+
+/// Scheduler information per core. Each core can run it's own scheduler code
+/// and thus they have their own data to refer to main scheduler structure and
+/// to hold their own information related to thread executed on this core.
+pub struct Core<C: Cpu> {
+    sched: RwLock<Sched<C>>,
+
+    thread: ThreadKey,
+    pos: usize,
+
+    key: CoreKey,
 }
 
 /// Type to identify core.
@@ -28,23 +75,40 @@ pub trait Cpu {
     /// Get key of core where scheduler executes.
     fn master_core(&self) -> CoreKey;
 
-    /// Attach thread to given core.
-    fn attach_thread(&mut self, core: CoreKey, thread: &ThreadSched);
+    /// Add new timer to countdown the time. Given time is in microseconds.
+    fn add_timer(&mut self, time: usize, timer_type: TimerType);
 
-    /// Set next timer to countdown the time. Given time is in Hz = 1/freq sec.
-    fn set_timer(&mut self, freq: u16);
+    /// Join thread and exit execution of scheduler code.
+    fn join(&mut self, mode: JoinMode) -> !;
+}
 
-    /// Join thread and pause execution of scheduler code.
-    fn join(&mut self);
+/// Contains the queue of threads and a tree that allows to locate
+/// the thread in the queue by its key.
+#[derive(Default)]
+struct ThreadSet {
+    queue: Vec<ThreadSched>,
+    map: BTreeMap<ThreadKey, usize>,
 }
 
 pub struct Sched<C: Cpu> {
 
-    freq: u16,
+    /// The time of how long a thread normally should execute before switching
+    /// to other.
+    time: u32,
 
+    /// Maximal additional time that is allowed to be added for a thread
+    /// in a one execution cycle. In microseconds.
+    max_add_time: u32,
+
+    /// Active threads that are executed or sleeping threads that wait for
+    /// processor time.
     threads: Vec<ThreadSched>,
 
-    cur_thread_id: usize,
+    /// Threads that were removed from active thread list due to they wait
+    /// for external signal.
+    waiting_threads: ThreadSet,
+
+    next_thread_id: usize,
 
     /// CCS network controller.
     network: Network,
@@ -59,72 +123,15 @@ impl<C> Sched<C>
         Default::default()
     }
 
-    /// Frequency of internal timer that shows how often to change threads.
-    /// Measured in Hz.
-    pub fn frequency(&self) -> u16 {
-        self.freq.clone()
+    /// Internal timer value that shows how often to change threads.
+    /// Mesured in microseconds.
+    pub fn time(&self) -> u32 {
+        self.time.clone()
     }
 
     /// Vec of threads that are sleeping and waiting to be executed.
     pub fn threads(&self) -> &Vec<ThreadSched> {
         &self.threads
-    }
-
-    /// Start scheduling tasks and switching threads.
-    pub fn start(&mut self) -> ! {
-        loop {
-            self.switch_threads();
-            self.reset_timer();
-            self.join_thread();
-        }
-    }
-
-    /// Switch threads command.
-    fn switch_threads(&mut self) {
-        let core_count = self.cpu.core_count();
-        let threads_len = self.threads.len();
-
-        // How many threads to switch.
-        let switch_count = if core_count > threads_len {
-            threads_len
-        } else {
-            core_count
-        };
-
-        let mut remain = switch_count;
-        let mut cur_core = 0;
-        while remain > 0 {
-            // Get next thread id.
-            let prev_id = self.cur_thread_id;
-            let next_id = self.cur_thread_id + 1;
-            let next_id = if next_id > threads_len {
-                0
-            } else {
-                next_id
-            };
-            self.cur_thread_id = next_id;
-
-            // Attach thread to core.
-            self.cpu.attach_thread(cur_core, &self.threads[next_id]);
-
-            // Update state of the threads in the network.
-            self.network.active_thread(self.threads[next_id].key());
-            self.network.sleep_thread(self.threads[prev_id].key());
-
-            cur_core += 1;
-            remain -= 1;
-        }
-    }
-
-    /// Restart timer to count time for next switch.
-    fn reset_timer(&mut self) {
-        self.cpu.set_timer(self.freq);
-    }
-
-    /// Join the thread on master core and thus pause execution of scheduler
-    /// code until timer fires interrupt.
-    fn join_thread(&mut self) {
-        self.cpu.join();
     }
 }
 
@@ -133,26 +140,160 @@ impl<C> Default for Sched<C>
 
     fn default() -> Self {
         Sched {
-            freq: 100,
-            threads: Default::default(),
-            cur_thread_id: Default::default(),
-            active_thread_ids: Default::default(),
-            network: Default::default(),
-            cpu: Box::new(C::init()),
+            time            : 5_000, // Same as 200 Hz.
+            max_add_time    : 2_500,
+            waiting_threads : Default::default(),
+            threads         : Default::default(),
+            next_thread_id  : Default::default(),
+            network         : Default::default(),
+            cpu             : Box::new(C::init()),
         }
+    }
+}
+
+impl<C> Core<C> where C: Cpu {
+
+    /// Interrupt of thread execution by timeout signal.
+    pub fn timeout_interrupt(&mut self) -> ! {
+        // Get next thread to execute.
+        let (prev_pos, next_pos) = {
+            let mut schedw = self.schedw();
+            let next_pos = schedw.next_thread_id.clone();
+
+            // Update next thread counter.
+            let prev_pos = schedw.next_thread_id;
+            schedw.next_thread_id += 1;
+            if schedw.next_thread_id >= schedw.threads.len() {
+                schedw.next_thread_id = 0;
+            }
+
+            (prev_pos, next_pos)
+        };
+        let schedr = self.schedr();
+        let next = &schedr.threads[next_pos].key.clone();
+        let prev = &schedr.threads[prev_pos].key.clone();
+        drop(schedr);
+
+        self.thread = next.clone();
+        self.pos = next_pos;
+
+        // Update thread state in the network.
+        self.schedw().network.active_thread(&next);
+        self.schedw().network.sleep_thread(&prev);
+
+        // Update timer.
+        let time = {
+            let schedr = self.schedr();
+            let add_time = schedr.threads[next_pos].add_time;
+
+            let add_time = if add_time > schedr.max_add_time {
+                schedr.max_add_time.clone()
+            } else {
+                add_time
+            };
+
+            (schedr.time + add_time) as _
+        };
+        self.schedw().cpu.add_timer(time, TimerType::Execution);
+
+        self.join(JoinMode::ContextChanged);
+    }
+
+    /// Thread now waits for channel signal with timer. Time accepted in
+    /// microseconds.
+    pub fn channel_wait_with_timer_interrupt(&mut self, time: usize,
+        channel_key: ChannelKey
+    ) -> ! {
+        // All participants that waited for this channel must be woke up
+        // except current thread which goes to waiting state.
+        let thread = self.thread.clone();
+        let wake_up_list = self.schedw().network
+                .channel_signal(&thread, &channel_key, true).unwrap();
+
+        for thread_key in wake_up_list {
+            let mut schedw = self.schedw();
+            let thread_sched = schedw.waiting_threads.remove_key(&thread_key);
+            schedw.threads.push(thread_sched);
+        }
+
+        self.timeout_interrupt()
+    }
+
+    /// Thread was waiting for the channel signal but timer has reached
+    /// the timeout.
+    pub fn channel_wait_timeout_interrupt(&mut self, key: ThreadKey,
+        timer_remain: usize
+    ) -> ! {
+        // Update the network.
+        self.schedw().network.sleep_thread(&key);
+
+        // Move thread to the list of the threads that wait for processor time.
+        // And add remaining time to thread.
+        let mut thread_sched = self.schedw().waiting_threads.remove_key(&key);
+        thread_sched.add_time += timer_remain as u32;
+        self.schedw().threads.push(thread_sched);
+
+        // Execute next thread.
+        self.timeout_interrupt()
+    }
+
+    // Read access to scheduler.
+    fn schedr(&self) -> RwLockReadGuard<Sched<C>> {
+        self.sched.read().unwrap()
+    }
+
+    /// Read-write access to scheduler.
+    fn schedw(&mut self) -> RwLockWriteGuard<Sched<C>> {
+        self.sched.write().unwrap()
+    }
+
+    fn join(&self, mode: JoinMode) -> ! {
+        // Use unsafe to get access to CPU and drop schedr so other cores
+        // could access Sched.
+        let cpu = unsafe {
+            let ptr = &self.schedr().cpu as *const Box<C> as *mut Box<C>;
+            &mut *ptr
+        };
+        cpu.join(mode)
     }
 }
 
 impl ThreadSched {
 
-    pub fn new(key: ThreadKey) -> Self {
-        ThreadSched {
-            key,
+    pub fn key(&self) -> &ThreadKey {
+        &self.key
+    }
+}
+
+impl ThreadSet {
+
+    /// Remove given thread key.
+    pub fn remove_key(&mut self, key: &ThreadKey) -> ThreadSched {
+        let pos = self.map.remove(key).unwrap();
+        let last = if self.queue.len() == 1 {
+            None
+        } else {
+            Some(self.queue.len() - 1)
+        };
+
+        if last.is_some() {
+            let last = last.unwrap();
+            let last_sched = self.queue.swap_remove(pos);
+
+            // Update changed element position in the tree.
+            let moved_element = self.map.get_mut(&last_sched.key).unwrap();
+            *moved_element = pos;
+            last_sched
+        } else {
+            self.queue.remove(pos)
         }
     }
 
-    pub fn key(&self) -> &ThreadKey {
-        &self.key
+    /// Add new thread scheduling information.
+    pub fn add_key(&mut self, ts: ThreadSched) {
+        let key = ts.key.clone();
+        self.queue.push(ts);
+        self.map.insert(key, self.queue.len() - 1);
     }
 }
 
